e3c535bfa238eab769d79b2667d4bdc89f62fdac
Marilia Arantes Lima <arantes.marilia@hotmail.com>
2025-07-10 09:17:24 -0400

    first commit

---
+++ cypress/e2e/loginspec.cy.js
@@ -0,0 +1,17 @@
+describe('Orange test', () => {
+  it('login com sucesso', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get("[name='username']").type('Admin')
+    cy.get("[name='password']").type('admin123')
+    cy.get("[type='submit']").click()
+    cy.location('pathname').should('equal','/web/index.php/dashboard/index')
+    cy.get('.oxd-topbar-header-breadcrumb-module').contains('Dashboard')
+  })
+  it('login fail', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get("[name='username']").type('Test')
+    cy.get("[name='password']").type('Test')
+    cy.get('.oxd-button').click()
+    cy.get('.oxd-alert')
+  })
+})
